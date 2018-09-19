34d7737b685f06d2fcf4ec0fc1c70722017af89b
Carlos Sánchez Cazorla <cjsanchez@uloyola.es>
2018-09-19 12:46:53 +0200

    First commit

---
+++ Assets/ShowMetrics.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowMetrics : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void FixedUpdate () {
+		Rigidbody rb = this.GetComponent<Rigidbody> ();
+		if(!Mathf.Approximately(rb.velocity.x,0.0f))
+		{
+			Debug.Log ("Velocity X: " + rb.velocity.x);		
+		}
+
+	}
+}
---
+++ Assets/shuffleboard.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shuffleboard : MonoBehaviour {
+
+	public Vector3 initialSpeed = new Vector3(8.5f,0,0);
+
+	// Use this for initialization
+	void Start () {
+		Rigidbody rb = this.GetComponent<Rigidbody> ();
+
+		rb.velocity = initialSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
